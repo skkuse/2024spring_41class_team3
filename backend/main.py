cef8b84101748d31d4a00aef7f58b2c0ab6ab878
baerini <rudduqlek@naver.com>
2024-05-10 16:31:06 +0900

    first

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# cover page
 @app.get("/")
 def root():
     return {"message":"hello"}
 
+# input code (post)
+# before.java co2 => filtering 1, 2, 3 => time check, after.java co2 => db commit => return
 
-# home
+# bulletin
 
-# api1. input code
-# return "co2"
+# bulletin detail
