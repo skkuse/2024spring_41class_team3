d268a874dc05f81fa015c6de515212c5c56fce0a
Ihm-hyun-sir <64261131+Ihm-hyun-sir@users.noreply.github.com>
2024-05-13 12:14:49 +0900

    Delete pythonserver directory

--- pythonserver/example3/src/Fixed.java
+++
@@ -1,2 +0,0 @@
-import java.util.Random;public class Fixed {
-    public static void main(String[] args) {	    Random r1 = new Random();        for(int i=0; i<10; i++) {            int num = r.nextInt(10);            System.out.println(num);        }    }}
