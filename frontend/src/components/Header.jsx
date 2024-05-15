e7ce126a14027e50372e2aa173c57700ae120194
youznn <leejin914418@gmail.com>
2024-05-16 00:13:44 +0900

    css 수정

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,17 +5,14 @@ import { useNavigate } from 'react-router';
 import HomeImage from '@/images/green-coding.png';
 import BulletinImage from '@/images/bulletin.png';
 
-
-
-
 export default function Header() {
 	const navigate = useNavigate();
 	const onHomeButtonClick = () => {
-			navigate(`/`);
+		navigate(`/`);
 	};
 
 	const onBulletinButtonClick = () => {
-			navigate('/list');
+		navigate('/list');
 	};
 
 	const home = HomeImage;
@@ -23,20 +20,36 @@ export default function Header() {
 
 	return (
 		<>
-			<div className='relative isolate flex bg-gray-50 flex justify-between px-10 py-1'>
-			<Button variant="ghost" onClick={onHomeButtonClick}><img src={home} alt="Placeholder" style={{ width: '48px', height: '48px' }}/></Button>
-			<p className="text-sm leading-6">
-				<strong className="font-semibold">Green coding</strong>
-				<svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
-				</svg>
-			  </p>
-			  <a
-				className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-			  >
-				<Button variant="ghost" onClick={onBulletinButtonClick}><img src={bulletin} alt="Placeholder" style={{ width: '36px', height: '36px' }}/>Bulletin</Button> <span aria-hidden="true">&rarr;</span>
-			  </a>
+			<div className="flex h-20 items-center justify-between bg-gray-50 px-10 py-1">
+				<Button
+					variant="ghost"
+					className="rounded-full p-8 hover:bg-lime-700/15"
+					onClick={onHomeButtonClick}
+				>
+					<img
+						src={home}
+						alt="Placeholder"
+						style={{ width: '64px', height: '64px' }}
+					/>
+				</Button>
+				<p className="text-4xl font-semibold text-lime-800">GREEN CODING</p>
+				<div className="flex rounded-full bg-lime-600/80 px-3.5 py-2 text-xl font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
+					<Button
+						variant="ghost"
+						className="flex gap-2 bg-transparent text-xl hover:bg-transparent"
+						onClick={onBulletinButtonClick}
+					>
+						<img
+							src={bulletin}
+							alt="Placeholder"
+							style={{ width: '36px', height: '36px' }}
+						/>
+						Bulletin
+						<p className="text-2xl">&rarr;</p>
+					</Button>
+				</div>
 			</div>
-			<Separator className="bg-gradient-to-r from-lime-500 to-yellow-100 h-1"/>
+			<Separator className="h-1 bg-gradient-to-r from-lime-500 to-yellow-100" />
 		</>
 	);
 }
