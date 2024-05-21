184f1277419ad96cf510360b256e1afbfdea815a
unknown <nazny5@g.skku.edu>
2024-05-21 13:19:03 +0900

    logics

---
+++ backend/logics/example2/src/Fixed.java
@@ -0,0 +1,6 @@
+public class Fixed {
+    public static void main(String[] args) {        boolean cond1 = checkCond1();        boolean cond2 = checkCond2();        boolean cond3 = checkCond3();		if((cond1 && cond2) && cond3) {
+			                    System.out.println("Hello");
+
+		}
+    }    public static boolean checkCond1() {        int sum = 0;        for(int i=0; i<10000; i++) {            sum += i;        }        return sum > 50000;    }    public static boolean checkCond2() {        int sum = 0;        for(int i=0; i<10000; i++) {            sum *= i;        }        return sum > 50000;    }    public static boolean checkCond3() {        int sum = 0;        for(int i=0; i<10000; i++) {            sum *= i;        }        return sum > 50000;    }}
