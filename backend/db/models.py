d2c7d6ccd97d7fb115b5ec04c17d0b46580096d5
baerini <rudduqlek@naver.com>
2024-05-24 23:11:05 +0900

    model change

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, Date
+# 모델 정의
+from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from .database import Base
 
 class Code(Base):
@@ -8,6 +9,14 @@ class Code(Base):
     before = Column(String)
     after = Column(String)
     github_id = Column(String, nullable=True)
+    country_id = Column(Integer, ForeignKey('country.country_id'))
+
+class Mapping(Base):
+    __tablename__ = "mapping"
+    
+    mapping_id = Column(Integer, primary_key=True)
+    code_id = Column(Integer, ForeignKey('code.code_id'))
+    algorithm_id = Column(Integer)
 
 class Country(Base):
     __tablename__ = "country"
@@ -22,3 +31,4 @@ class Visitor(Base):
     visitor_id = Column(Integer, primary_key=True)
     date = Column(Date)
     daily_visitor = Column(Integer)
+    
