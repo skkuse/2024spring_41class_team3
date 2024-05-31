5f4a55ae5039684b5607fd54f9c6b478e0acb37f
root <root@ip-172-31-17-159.ec2.internal>
2024-05-31 16:57:49 +0000

    배포후 머지 전

--- backend/logics/Temp.java
+++ backend/logics/Temp.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class Temp {
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<>(100000000);
-        for(int i = 0 ; i < 100000000; i++)
+        ArrayList<String> arr = new ArrayList<>(10);
+        for(int i = 0 ; i < 10; i++)
             arr.add("Hello");
         for(int i=0; i<arr.size(); i++) {
             arr.get(i);
