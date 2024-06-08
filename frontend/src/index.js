e2dbec1dcf5370ffd0190031673659f7f4eb7fc1
junhyeong <asdfg3128@gmail.com>
2024-06-08 21:52:27 +0900

    frontend 수정
    Index.js 수정,
    carco.config.js 수정,
    build 파일 생성

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import MainPage from './pages/MainPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/app" element={<App />} />
+      {/* 다른 경로를 추가할 수 있습니다. */}
+    </Routes>
+  </Router>
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
